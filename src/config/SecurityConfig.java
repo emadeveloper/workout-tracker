b9b425e507bf35bd3b2e6dad8e1ea92530172e41
Emanuel Martinez <yinproduccuiones@gmail.com>
2025-01-30 16:17:30 -0300

    initial structure

---
+++ src/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.fitness.workout_tracker;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    // We'll add security configurations here later
+}
